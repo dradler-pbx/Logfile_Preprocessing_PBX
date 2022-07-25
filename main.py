12e74181b4e3e6be71f91ee2a369b6fcc25a62c5
dradler-pbx <dominik.radler@productbloks.com>
2022-07-25 16:00:43 +0200

    fist official release

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import subprocess
 import io
 
 
-data = pd.DataFrame()
 dev_info = {}
 logfiles = []
 config = {}
@@ -63,32 +62,33 @@ def check_logfiles():
     for file in logfiles:
         file_array = file.split(".")[0]
         file_array = file_array.split("-")
+        dev_id = '-'.join(file_array[0:2])
+        if not dev_id in dev_info.keys():
+            dev_info[dev_id] = {}
+            dev_info[dev_id]['type'] = file_array[0]
+            dev_info[dev_id]['sn'] = file_array[1]
+            dev_info[dev_id]['lf'] = file_array[2]
+            dev_info[dev_id]['files'] = []
 
-        if first_run:
-            dev_info['type'] = file_array[0]
-            dev_info['SN'] = file_array[1]
-            dev_info['LF_version'] = file_array[2]
-            first_timestamp = file_array[-1]
+        if file_array[2] != dev_info[dev_id]['lf']:
+            raise ValueError('Cannot operate on different logfile versions for the same serial number!')
 
-            first_run = False
-
-        if file_array[0] != dev_info['type']:
-            raise ValueError('Found files of different types!')
-            break
-        if file_array[1] != dev_info['SN']:
-            raise ValueError('Found files of different units!')
-            break
-        if file_array[2] != dev_info['LF_version']:
-            raise ValueError('Found files with different logfile versions!')
-            break
+        dev_info[dev_id]['files'].append(file)
 
     msg = []
     msg.append(text_break())
-    msg.append('Device: {}'.format('-'.join([dev_info['type'], dev_info['SN']])))
-    msg.append('Logfile count: {}'.format(len(logfiles)))
-    msg.append('First timestamp: {}'.format(first_timestamp))
-    msg.append('Last timestamp: {}'.format(file_array[-1]))
     msg.append(text_break())
+    msg.append('CHECK INFO:')
+    for dev in dev_info:
+        msg.append('Device: {}'.format(dev))
+        msg.append('File count: {}'.format(len(dev_info[dev]['files'])))
+        dev_info[dev]['files'] = sorted(dev_info[dev]['files'])
+        first_timestamp = dev_info[dev]['files'][0].split('.')[0].split('-')[-1]
+        last_timestamp = dev_info[dev]['files'][-1].split('.')[0].split('-')[-1]
+        msg.append('First file: {}'.format(first_timestamp))
+        msg.append('Last file: {}'.format(last_timestamp))
+        msg.append(text_break())
+
     msg = '\n'.join(msg)
     check_label_text.set(msg)
 
@@ -107,59 +107,73 @@ def exchange_header(file_header, lf_version):
 
 
 def read_logfiles():
-    global data
+    global dev_info
     msg = check_label_text.get()
     # generate a list of logfile names as logfiles[]
-    msg = msg + '\nReading {} logfiles.'.format(len(logfiles))
-    check_label_text.set(msg)
+    msg_list = [msg]
+    msg_list.append(text_break())
+    msg_list.append('READING FILES')
+    check_label_text.set('\n'.join(msg_list))
 
-    df_list = []
+    for dev in dev_info:
+        msg_list.append(' ')
+        msg_list.append('Reading {}'.format(dev))
+        check_label_text.set('\n'.join(msg_list))
 
-    for file in logfiles:
-        filepath = config["logfile_folder"] + file
-        print(filepath)
-        df = pd.read_csv(filepath, sep=";")
-        df_list.append(df)
+        df_list = []
+        data = pd.DataFrame()
 
-    # concatenate the list of df to one single df
-    data = pd.concat(df_list, axis=0, ignore_index=True)
+        for file in dev_info[dev]['files']:
+            filepath = config["logfile_folder"] + file
+            df = pd.read_csv(filepath, sep=";")
+            df_list.append(df)
 
-    # exchange the header with clear name header
-    data.columns = exchange_header(data.columns, dev_info['LF_version'])
+        # concatenate the list of df to one single df
+        data = pd.concat(df_list, axis=0, ignore_index=True)
 
-    # convert timestamp to datetime
-    data['timestamp_UNIXms'] = pd.to_datetime(data['timestamp_UNIXms'], unit='ms')
+        # exchange the header with clear name header
+        data.columns = exchange_header(data.columns, dev_info[dev]['lf'])
 
-    # replace 'T' and 'F' with True and False and Nan with np.nan
-    data.replace({'T': True, 'F': False, 'NaN': np.nan}, inplace=True)
+        # convert timestamp to datetime
+        data['timestamp_UNIXms'] = pd.to_datetime(data['timestamp_UNIXms'], unit='ms')
+
+        # replace 'T' and 'F' with True and False and Nan with np.nan
+        data.replace({'T': True, 'F': False, 'NaN': np.nan}, inplace=True)
+
+        # append the data into the dictionary
+        dev_info[dev]['data'] = data
 
     # activate write button
     export_btn.state(["!disabled"])
 
     # write completion message
-    msg = msg + '\nFinished reading {} logfiles.'.format(len(logfiles))
-    msg = msg + text_break('\n')
-    check_label_text.set(msg)
+    msg_list.append(text_break())
+    msg_list.append('Finished reading.')
+    check_label_text.set('\n'.join(msg_list))
 
-    # write describe analysis of logfiles to check label
-    describe_str = print_to_string(data['timestamp_UNIXms'].diff().astype('timedelta64[ms]').describe(percentiles=[0.05, 0.25, 0.5, 0.75, 0.95]))
-    msg = msg + '\nTimestamp difference analysis (in milliseconds):\n'
-    msg = msg + describe_str
-    check_label_text.set(msg)
-    print(describe_str)
+    # # write describe analysis of logfiles to check label
+    # describe_str = print_to_string(data['timestamp_UNIXms'].diff().astype('timedelta64[ms]').describe(percentiles=[0.05, 0.25, 0.5, 0.75, 0.95]))
+    # msg = msg + '\nTimestamp difference analysis (in milliseconds):\n'
+    # msg = msg + describe_str
+    # check_label_text.set(msg)
+    # print(describe_str)
 
     # enable export button
     export_btn.configure(state="!disabled")
 
 
 def export_data():
-    first_timestamp = data['timestamp_UNIXms'].iloc[0].strftime('%y%m%d_%H%M%S')
-    last_timestamp = data['timestamp_UNIXms'].iloc[-1].strftime('%y%m%d_%H%M%S')
+    export_text_list = []
+    for dev in dev_info:
+        data = dev_info[dev]['data']
+        first_timestamp = data['timestamp_UNIXms'].iloc[0].strftime('%y%m%d_%H%M%S')
+        last_timestamp = data['timestamp_UNIXms'].iloc[-1].strftime('%y%m%d_%H%M%S')
 
-    filename = "-".join([dev_info['type'], dev_info['SN'], 'export', first_timestamp, 'to', last_timestamp]) + ".csv"
-    data.to_csv(filename, index=False)
+        filename = "-".join([dev_info[dev]['type'], dev_info[dev]['sn'], 'export', first_timestamp, 'to', last_timestamp]) + ".csv"
+        data.to_csv(filename, index=False)
 
-    export_label_text.set(filename + ' exported.')
+        export_text_list.append(filename + ' exported.')
+    export_label_text.set('\n'.join(export_text_list))
 
 
 def open_logfile_folder():
@@ -183,14 +197,18 @@ export_label_text = tk.StringVar(value='Export label text...')
 label_style = ttk.Style()
 label_style.configure('label.TLabel')
 
+header_style = ttk.Style()
+header_style.configure('header.TLabel', padding="8p")
+
 btn_style = ttk.Style()
 btn_style.configure('btn.TButton', padding="4p")
 
 btn_frame_style = ttk.Style()
-btn_frame_style.configure('btn_frame.TFrame', padding="4p", background = "red")
+btn_frame_style.configure('btn_frame.TFrame', padding="4p")
 
-header = ttk.Label(root, text="header")
-header.grid(column=0, row=0, columnspan=2, sticky="NSEW")
+logo = ImageTk.PhotoImage(Image.open('source/PBX_Logo_black_small.png'))
+header = ttk.Label(root, image=logo, style='header.TLabel')
+header.grid(column=0, row=0, columnspan=2, sticky="E")
 
 sep1 = ttk.Separator(root, orient='horizontal')
 sep1.grid(row=1, column=0, columnspan=2, sticky="EW")
